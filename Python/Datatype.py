3dcfd3b4de275e68f043471dd01a5e11b486d4bd
Jemish Dobariya <Jemishdobariya9@gmail.com>
2025-08-12 11:44:08 +0530

    Python: Add initial scripts

---
+++ Python/Datatype.py
@@ -0,0 +1,33 @@
+# Python Datatypes with Examples
+
+# Integer
+num = 10
+print("Integer:", num)
+
+# Float
+price = 19.99
+print("Float:", price)
+
+# String
+name = "Alice"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
+
+# List
+fruits = ["apple", "banana", "cherry"]
+print("List:", fruits)
+
+# Tuple
+coordinates = (10, 20)
+print("Tuple:", coordinates)
+
+# Dictionary
+person = {"name": "Bob", "age": 25}
+print("Dictionary:", person)
+
+# Set
+unique_numbers = {1, 2, 3, 2}
+print("Set:", unique_numbers)
---
+++ Python/HelloWorld.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+# This is a simple Python script that prints "Hello World!" to the console.
+# You can run this script using Python interpreter. 
